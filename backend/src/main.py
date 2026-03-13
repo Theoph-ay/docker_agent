2a1fdaed7f708245ad9f540cc49ab9102f0eab3b
Olayiwola Theophilus <Olayiwolatheophilusayomide@gmail.com>
2026-03-13 13:04:16 +0100

    fixed crash

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,8 +5,6 @@ app = FastAPI()
 
 MY_PROJECT = os.environ.get("MY_PROJECT") or "This is my project"
 API_KEY = os.environ.get("API_KEY")
-if not API_KEY:
-    raise NotImplementedError("API_KEY is not set")
 
 @app.get("/")
 def read_index():
