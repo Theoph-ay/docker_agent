a0b9f0f000b4ed3b70859992f4643c94a42adbd2
Olayiwola Theophilus <Olayiwolatheophilusayomide@gmail.com>
2026-03-13 17:40:33 +0100

    db path issue fix

--- backend/src/api/db.py
+++ backend/src/api/db.py
@@ -8,7 +8,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 if DATABASE_URL == "":
     raise ValueError("DATABASE_URL needs to be set")
 
-DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql+psycopg")
+DATABASE_URL = DATABASE_URL.replace("postgres://", "postgres+psycopg://")
 
 engine = sqlmodel.create_engine(DATABASE_URL)
 
