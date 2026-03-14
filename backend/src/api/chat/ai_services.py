1edab5b8082b2794fe323ffc6772c669858fd497
Olayiwola Theophilus <Olayiwolatheophilusayomide@gmail.com>
2026-03-14 10:15:00 +0100

    corrected a tuple and set error with langchain messages

--- backend/src/api/chat/ai_services.py
+++ backend/src/api/chat/ai_services.py
@@ -7,7 +7,7 @@ from langchain_groq import ChatGroq
 class EmailMessage(BaseModel):
     subject: str
     contents: str
-    invalid_request: bool | = Field(default=False)
+    invalid_request: bool = Field(default=False)
 
 GROQ_API_KEY=os.getenv("GROQ_API_KEY")
 MODEL_NAME=os.getenv("MODEL_NAME")
@@ -22,14 +22,14 @@ llm_base = ChatGroq(**groq_params)
 llm = llm_base.with_structured_output(EmailMessage)
 
 messages = [
-    {
+    (
         "system",
-        "You are an helpful assistant for a research composing plaintext emails. Do not use markdown in yout response"
-    },
-    {
+        "You are a helpful assistant for research composing plaintext emails. Do not use markdown."
+    ),
+    (
         "human",
-        "Create an email about the benefits of coffee. Do not use markdown in your response only plaintext"
-    }
+        "Create an email about the benefits of coffee. Only plaintext."
+    )
 ]
 
 response = llm.invoke(messages)
